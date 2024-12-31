a9c2949a5d4b8cb9cea15c9253253b47b2af0caa
Shadman Shaikh <shadmanforreal45@gmail.com>
2024-12-31 20:37:23 +0400

    added collision beams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { Timeline } from "@/components/ui/timeline";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 export default function BackgroundLinesDemo() {
@@ -171,6 +172,21 @@ export default function BackgroundLinesDemo() {
         Building Ideas to reality leveraging code & coffee 🍵
       </p>
     </BackgroundLines>
+
+    <BackgroundBeamsWithCollision>
+      <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+        Want to see Full Stack Magic?{" "}
+        <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+            <span className="">Full Stack Wizardry.</span>
+          </div>
+          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+            <span className="">Full Stack Wizardry.</span>
+          </div>
+        </div>
+      </h2>
+    </BackgroundBeamsWithCollision>
+
 
     <Timeline data={data} />
 </div>
